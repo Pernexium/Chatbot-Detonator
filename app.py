3a8366ae1fa37cc39d8bb72d98903fde3f12bf01
HibranTapiaAtPernexium <157671037+HibranTapiaAtPernexium@users.noreply.github.com>
2024-10-29 20:33:35 -0600

    arreglo de fechas

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ def subir_base(contact_type):
         bytes_data = uploaded_file.read()
         
         tz_mexico = pytz.timezone('America/Mexico_City')
-        hoy = datetime.now() - pd.Timedelta(hours=6)
+        hoy = datetime.now() - timedelta(hours=6)
         year_month = hoy.now().strftime("%Y_%m")
         day_month = hoy.now().strftime("%m_%d")
         
